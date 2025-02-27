95c030cb1871ffc03d2b854d0eaed0772ebed751
CoreMechanicsDev <coremechanics@icloud.com>
2025-02-27 00:01:30 -0800

    Adding more file locks types

--- Source/cm_dev_test/MyMacros.h
+++ Source/cm_dev_test/MyMacros.h
@@ -1,19 +1,3 @@
-#pragma once
-
-// Git test comment modification
-
-#include "DrawDebugHelpers.h"
-
-// Macro to print text to screen
-#define PRINT(DebugMessage, ...){ GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Green, FString::Printf(TEXT(DebugMessage)));}
-
-// Macro to print to screen which ALSO takes argument to allow things like printing variables / function names
-#define PRINTARGS(DebugMessage, ...){ GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Green, FString::Printf(TEXT(DebugMessage), __VA_ARGS__));}
-
-// Macros to draw centered sphere which only draw if a valid world object is provided (one persistent, 1 not)
-#define SPHERE(Center) if (GetWorld()) DrawDebugSphere(GetWorld(), Center, 20, 16, FColor::Blue, true);
-#define SPHERE_TICK(Center) if (GetWorld()) DrawDebugSphere(GetWorld(), Center, 20, 16, FColor::Green, false);
-
-// Same as above but for LINE
-#define LINE(LineStart, LineEnd) if (GetWorld()) DrawDebugLine(GetWorld(), LineStart, LineEnd, FColor::Blue, true);
-#define LINE_TICK(LineStart, LineEnd) if (GetWorld()) DrawDebugLine(GetWorld(), LineStart, LineEnd, FColor::Green, false);
+version https://git-lfs.github.com/spec/v1
+oid sha256:2de1aef6e984f1df64e7b3b4c4ede91013b68bc9f8388ae322e2fa4158bceb59
+size 1109
