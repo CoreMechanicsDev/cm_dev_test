9d7a09abd8ac66f1d8bd88a8be0bd03ffe8b1303
CoreMechanicsDev <coremechanics@icloud.com>
2026-03-08 01:50:12 -0800

    Updating to 5.7

--- Source/cm_dev_testEditor.Target.cs
+++ Source/cm_dev_testEditor.Target.cs
@@ -8,8 +8,8 @@ public class cm_dev_testEditorTarget : TargetRules
 	public cm_dev_testEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("cm_dev_test");
 	}
 }
