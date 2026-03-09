676221dc9a353f7a6a4488ab6554208fa16fa0d6
CoreMechanicsDev <coremechanics@icloud.com>
2026-03-09 01:10:04 -0700

    Updating Target Settings to 5.7

--- Source/cm_dev_test.Target.cs
+++ Source/cm_dev_test.Target.cs
@@ -9,7 +9,7 @@ public class cm_dev_testTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("cm_dev_test");
 	}
 }
